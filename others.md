0d094caa913d9f9053b8675283a5bac1d9ecbfc8
Antonio Ramos <ramos.antonio.dev@gmail.com>
2022-07-24 18:39:26 -0300

    fix: ajustes de configurações

--- others.md
+++
@@ -1,2 +0,0 @@
-<!-- Príncipio da modelagem do banco de dados -->
-https://excalidraw.com/#json=-x1slz21xkN5304hGC_0S,ymbDqFH08MLYbK8LthN7jg
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const assetsRoutes = require("./routes/assets.routes");
 const loginRoutes = require("./routes/login.routes");
 const userRoutes = require("./routes/user.routes");
 const investimentosRoutes = require("./routes/investimentos.routes");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(userRoutes);
 app.use(loginRoutes);
